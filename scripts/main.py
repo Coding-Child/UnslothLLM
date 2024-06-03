116eff658a82bd170b4d548fa1f86aff938847aa
Suhyeong <pshpulip40@gmail.com>
2024-06-04 00:45:09 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,16 @@
 import os
 import random
 import numpy as np
-from sklearn.metrics import f1_score
 from datetime import datetime
 
 import torch
 
 from transformers import EarlyStoppingCallback, TrainingArguments
 
-from datasets import load_dataset, load_metric
-from torcheval.metrics import Perplexity
+from datasets import load_dataset
+from evaluate import load
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from unsloth import is_bfloat16_supported
 
 from model.LLM import load_model
 from Dataset.MedDialogueDataset import generate_prompt_in_batch
@@ -18,43 +18,40 @@ os.environ["WANDB_PROJECT"] = "llm_training"
 os.environ["WANDB_LOG_MODEL"] = "checkpoints"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+bleu = load('bleu')
+meteor = load('meteor')
+rouge = load('rouge')
 
-def compute_metrics(eval_pred, tokenizer):
-    bleu = load_metric('bleu')
-    meteor = load_metric('meteor')
-    rouge = load_metric('rouge')
-    perplexity = Perplexity(ignore_index=-100)
 
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
+def compute_metrics(pred, tokenizer):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions[0]
 
-    valid_ids = labels != -100
-    valid_predictions = predictions[valid_ids]
-    valid_labels = labels[valid_ids]
+    labels_ids = np.where(labels_ids != -100, labels_ids, tokenizer.pad_token_id)
 
-    predictions = [tokenizer.tokenize(tokenizer.decode(ids, skip_special_tokens=True)) for ids in valid_predictions]
-    references = [[tokenizer.tokenize(tokenizer.decode([label], skip_special_tokens=True))] for label in valid_labels]
+    pred_ids = np.where(pred_ids != -100, pred_ids, tokenizer.pad_token_id)
+    
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
 
-    perplexity.update(torch.tensor(logits), torch.tensor(labels))
+    bleu4_output = bleu.compute(predictions=pred_str, references=[[ref] for ref in label_str], max_order=4)
+    bleu2_output = bleu.compute(predictions=pred_str, references=[[ref] for ref in label_str], max_order=2)
+    meteor_output = meteor.compute(predictions=pred_str, references=label_str)
+    rouge_output = rouge.compute(predictions=pred_str, references=label_str)
 
-    bleu2_score = bleu.compute(predictions=predictions, references=references, max_order=2)['bleu']
-    bleu4_score = bleu.compute(predictions=predictions, references=references, max_order=4)['bleu']
-    meteor_score = meteor.compute(predictions=predictions, references=references)['meteor']
-    rouge = rouge.compute(predictions=predictions, references=references)
-    f1 = f1_score(valid_labels, valid_predictions, average='macro')
-    ppl = perplexity.compute().item()
+    return {'bleu4': round(bleu4_output['bleu'], 4), 
+            'bleu2': round(bleu2_output['bleu'], 4),
+            'meteor': round(meteor_output['meteor'], 4),
+            'rouge1': round(rouge_output['rouge1'], 4),
+            'rouge2': round(rouge_output['rouge2'], 4),
+            'rougeL': round(rouge_output['rougeL'], 4),
+            'rougeLsum': round(rouge_output['rougeLsum'], 4)
+           }
 
-    return {'bleu2': bleu2_score,
-            'bleu4': bleu4_score,
-            'meteor': meteor_score,
-            'rouge1': rouge['rouge1'].mid.fmeasure,
-            'rouge2': rouge['rouge2'].mid.fmeasure,
-            'rougeL': rouge['rougeL'].mid.fmeasure,
-            'rougeLsum': rouge['rougeLsum'].mid.fmeasure,
-            'f1': f1,
-            'perplexity': ppl
-            }
+def preprocess_logits_for_metrics(logits, labels):
+    pred_ids = logits.argmax(dim=-1)
 
+    return pred_ids, labels
 
 def seed_everything(seed):
     random.seed(seed)
@@ -107,7 +104,11 @@ def main(args):
                                                     )
 
     # Early stopping callback and scheduler
-    early_stopping = EarlyStoppingCallback(early_stopping_patience=num_epochs * 0.1)
+    if num_epochs <= 10:
+        early_stopping = EarlyStoppingCallback(early_stopping_patience=3)
+    else:
+        early_stopping = EarlyStoppingCallback(early_stopping_patience=15)
+
     if using_scheduler and sch_type == 'cosine':
         warmup_step = args.warmup_step
         scheduler_type = 'cosine'
@@ -121,13 +122,14 @@ def main(args):
     # Training the model
     training_args = TrainingArguments(per_device_train_batch_size=batch_size,
                                       gradient_accumulation_steps=accumulation_step,
+                                      gradient_checkpointing=True,
                                       warmup_steps=warmup_step,
                                       num_train_epochs=num_epochs,
                                       learning_rate=lr,
-                                      fp16=not torch.cuda.is_bf16_supported(),
-                                      bf16=torch.cuda.is_bf16_supported(),
+                                      fp16=not is_bfloat16_supported(),
+                                      bf16=is_bfloat16_supported(),
                                       logging_steps=5,
-                                      optim="paged_adamw_8bit",
+                                      optim="adamw_8bit",
                                       weight_decay=0.1,
                                       lr_scheduler_type=scheduler_type,
                                       do_eval=True,
@@ -135,7 +137,7 @@ def main(args):
                                       save_strategy='epoch',
                                       load_best_model_at_end=True,
                                       greater_is_better=False,
-                                      metric_for_best_model='perplexity',
+                                      metric_for_best_model='eval_loss',
                                       seed=3407,
                                       output_dir=args.save_path,
                                       logging_dir='./logs',
@@ -156,6 +158,7 @@ def main(args):
                          packing=False,
                          data_collator=data_collator,
                          compute_metrics=lambda x: compute_metrics(x, tokenizer),
+                         preprocess_logits_for_metrics=preprocess_logits_for_metrics,
                          callbacks=[early_stopping]
                          )
 
@@ -168,7 +171,8 @@ def main(args):
                         eval_dataset=dataset['test'],
                         packing=False,
                         data_collator=data_collator,
-                        compute_metrics=lambda x: compute_metrics(x, tokenizer)
+                        compute_metrics=lambda x: compute_metrics(x, tokenizer),
+                        preprocess_logits_for_metrics=preprocess_logits_for_metrics
                         )
 
     model.config.use_cache = False
